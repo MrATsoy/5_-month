f6064649c19e07eaefdbb4e3c9d4d808a4c2b0fc
MrATsoy <113983576+MrATsoy@users.noreply.github.com>
2023-02-13 22:25:13 +0600

    homework from one to five

---
+++ afisha/movie_app/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'text movie stars'.split()
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = 'title description duration director'.split()
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Director
+        fields = 'name movies_count'.split()
+
+
+class ShortReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'text stars'.split()
+
+
+class MoviesAndReviews(serializers.ModelSerializer):
+    movies_reviews = ShortReviewSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = 'title movies_reviews average_rate'.split()
+
---
+++ afisha/movie_app/views.py
@@ -0,0 +1,63 @@
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from .serializers import *
+from .models import *
+
+# Create your views here.
+
+
+class DirectorViewSet(viewsets.mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+@api_view(['GET'])
+def main_view(request, slug):
+    if slug == 'movies':
+        item = Movie.objects.all()
+        serializer = MovieSerializer(item, many=True)
+    elif slug == 'directors':
+        item = Director.objects.all()
+        serializer = DirectorSerializer(item, many=True)
+    elif slug == 'reviews':
+        item = Review.objects.all()
+        serializer = ReviewSerializer(item, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def main_detail_view(request, slug, id):
+    if slug == 'movies':
+        item = Movie.objects.filter(id=id)
+        serializer = MovieSerializer(item, many=True)
+    elif slug == 'directors':
+        item = Director.objects.filter(id=id)
+        serializer = DirectorSerializer(item, many=True)
+    elif slug == 'reviews':
+        item = Review.objects.filter(id=id)
+        serializer = ReviewSerializer(item, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def movies_and_reviews(request):
+    movie = Movie.objects.all()
+    serializer = MoviesAndReviews(movie, many=True)
+    return Response(data=serializer.data)
